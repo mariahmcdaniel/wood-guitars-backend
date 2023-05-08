74eee31130de123ec7932680c947332f49ccc380
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-07 21:28:46 -0400

    db entries

--- src/api/wood/controllers/wood.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * wood controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::wood.wood');
--- src/api/wood/services/wood.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * wood service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::wood.wood');
