834d8f80d62a10ee79ce1a29fe7d176c25911f2d
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-23 19:25:33 -0400

    configuring jawsdb

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: process.env.PORT || 3306,
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
