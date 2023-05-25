5f33d784838961ab47b83ab108c320372e2db476
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-24 20:49:51 -0400

    resolving errors

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -24,23 +24,21 @@
 
 const mysql = require("mysql2");
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-// }
-
-var connection = mysql.createConnection({
-  host: env('JAWSDB_HOST', 'localhost'),
-  port: process.env.PORT || 3306,
-  user: env('JAWSDB_USERNAME', 'strapi'),
-  dialect: 'mysql',
-  password: env("JAWSDB_PASSWORD", "strapi"),
-  database: env('JAWSDB_DATABASE', 'strapi'),
-  ssl: {
-    rejectUnauthorized: false
-  },
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: env('JAWSDB_HOST', 'localhost'),
+    port: env.int('JAWSDB_PORT', 3306),
+    user: env('JAWSDB_USERNAME', 'strapi'),
+    dialect: 'mysql',
+    password: env("JAWSDB_PASSWORD", "strapi"),
+    database: env('JAWSDB_DATABASE', 'strapi'),
+    ssl: {
+      rejectUnauthorized: false
+    },
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
