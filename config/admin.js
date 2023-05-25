e95a1aca4e92e6044e31ac02f0b0809e9f878af0
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-24 23:36:15 -0400

    config admin

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,11 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('STRAPI_API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT') || crypto.randomBytes(16).toString('base64'),
   },
   transfer: {
     token: {
