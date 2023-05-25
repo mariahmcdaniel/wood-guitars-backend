ff68d3ded0646f9ca06b1e3cc405bbe50c1e2ac9
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-25 00:35:40 -0400

    staged

--- config/admin.js
+++ config/admin.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'oVnFziDZp8FIxmgJppD0PA=='),
+    salt: env('API_TOKEN_SALT', 'STRAPI_API_TOKEN_SALT'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', 'ifWh4QdWqaAwvf5i5aHLjQ=='),
+      salt: env('TRANSFER_TOKEN_SALT', 'STRAPI_API_TOKEN_SALT'),
     },
   },
 });
